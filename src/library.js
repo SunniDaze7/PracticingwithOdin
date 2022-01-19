0221ed8f60713db8e2f0985dfb6fef5f68e606e1
SunniDaze7 <pnykii98@gmail.com>
2022-01-19 15:28:53 -0600

    Added functionailty to new book button

--- src/library.js
+++ src/library.js
@@ -2,7 +2,7 @@
 
 //querySelectors
 const bookCards = document.getElementById("book-cards")
-//const card = document.getElementById("card")
+const card = document.getElementById("card")
 const addBook = document.getElementById("new-book")
 const removeBook = document.getElementById("remove-book")
 const readBook = document.getElementById("read-book")
@@ -15,27 +15,35 @@ let userLibrary = [];
     this.author = author
     this.pages = pages
     this.read = read
-    this.info = function(){
-       return ("Title: " + title + " By: " + author + " Pages: " + pages + " " + read)
-    }
 }
 
 
 //get book info and add new book information to userLibrary array
  function addTolibrary(){
-    const newBook1 = new Book(prompt("Enter the book's title."), prompt("Who is this book's author?"), prompt("How many pages is this book?"), "I have read this!")
-    userLibrary.push(newBook1)
+    const newBook = new Book(prompt("Enter the book's title."), prompt("Who is this book's author?"), prompt("How many pages is this book?"), "I have read this!")
+    userLibrary.push(newBook)
     console.log(userLibrary)
-} 
-addBook.addEventListener("click", addTolibrary)
-
-
-//add book card to interface
-function displayCard(){
-        const bookCard = document.createElement('div')
-        bookCard.setAttribute("class", "card")
-        bookCards.appendChild(bookCard)
+    displayCard()
     }
+ 
+ 
+    addBook.addEventListener("click", addTolibrary)
+    
+let cardNumber = 0;
+
+//add book card and info to interface
+ function displayCard(){
+        const bookCard = document.createElement('div')
+        bookCard.setAttribute("id", "card")
+        bookCards.appendChild(bookCard)
+        bookCard.innerHTML = "Title: " + userLibrary[cardNumber].title
+        + "  Author: " + userLibrary[cardNumber].author
+        cardNumber++
+} 
 
 
-displayCard()
+
+
+
+
+
