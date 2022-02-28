33de4ec6c18a05ef993946b4e7d36e84fbf6e111
SunniDaze7 <pnykii98@gmail.com>
2022-02-27 18:18:51 -0600

    changed function syntax and made neater code

--- src/library.js
+++ src/library.js
@@ -5,42 +5,49 @@ const bookCards = document.getElementById("book-cards")
 const card = document.getElementById("card")
 const comment = document.getElementById("comment")
 const addBook = document.getElementById("new-book")
+const bookCardSection = document.getElementById("book-card-section")
 
 let userLibrary = [];
 
 //to construct a new book
- function Book(title, author, words){
+ function Book(title, author, words, date){
     this.title = title
     this.author = author
     this.words = words
+    this.date = date
 }
 
+let cardNumber = 0;
 
 //get book info and add new book information to userLibrary array
  function addTolibrary(){
-    const newBook = new Book(prompt("Enter the book's title."), prompt("Who is this book's author?"), prompt("Would you like to add the number of words? If so enter digits here."))
+    const newBook = new Book(prompt("Enter the book's title."), prompt("Who is this book's author?"), prompt("Would you like to add the number of words? If so enter digits here."), "unfinished")
     userLibrary.push(newBook)
     console.log(userLibrary)
     displayCard()
     }
  
-
     
-let cardNumber = 0;
 
 //add book card and info to interface
  function displayCard(){
      //book log
+        const bookCardSection = document.createElement("div")
+        bookCardSection.setAttribute("id", "book-card-section")
+        bookCardSection.setAttribute("data-number", cardNumber)
+        bookCardSection.style.display = "flex"
+        bookCardSection.style.flexDirection = "row"
         const bookCard = document.createElement('div')
         bookCard.setAttribute("id", "card")
+
     //comment section for book
         const bookComment = document.createElement('div')
         bookComment.setAttribute("id", "comment")
         const commentSection = document.createElement('div')
 
-        bookCards.appendChild(bookCard)
-        bookCards.appendChild(bookComment)
-        bookComment.appendChild(commentSection)
+        bookCards.appendChild(bookCardSection)
+        bookCardSection.appendChild(bookCard)
+        bookCardSection.appendChild(bookComment)
 
            //buttons
            const removeButton = document.createElement("button")
@@ -56,59 +63,45 @@ let cardNumber = 0;
         "<br/>Author: " + userLibrary[cardNumber].author + "<br/>Words: " + userLibrary[cardNumber].words 
         bookCard.appendChild(removeButton)
 
+
     //add info to book comment
         commentSection.innerHTML  = "Comment: "
         const dateSection = document.createElement('div')
         dateSection.setAttribute("id", "date-section")
-        dateSection.innerHTML = "Date Finshed: unfinished"
+        dateSection.innerHTML = "Date Finished: " + userLibrary[cardNumber].date
+        bookComment.appendChild(commentSection)
         bookComment.appendChild(readButton)
         bookComment.appendChild(dateSection)
 
-    //button functionality
-        removeButton.addEventListener("click", removeCard)
+
+    //remove button functionality
+        removeButton.addEventListener("click", function removeCard(){
+        bookCardSection = bookCardSection.dataset.number
+        bookCardSection.remove()
+        })
+
+
         readButton.addEventListener("click", readBook)
 
         cardNumber++
 } 
 
 
-//remove a book from interface
- function removeCard(){
-    const bookCard = document.getElementById("card")
-    const bookComment = document.getElementById("comment")
-    bookCard.remove()
-    bookComment.remove()
-}
-
-//add read status and date for book
+//add/remove read status and date for book
  function readBook(){
     const dateSection = document.getElementById("date-section")
     const bookComment = document.getElementById("comment")
     const readButton = document.getElementById("read-button")
-    //const finishDate = prompt("Great job finishing that book! What date did you finish it?")
 
-
-    dateSection.innerHTML = "Date Finished: " + prompt("Great job finishing that book! What date did you finish it?")
     bookComment.appendChild(dateSection)
+    dateSection.innerHTML = "Date Finished: " + prompt("Congratulations finishing that book! What date did you finish it?")
     readButton.innerHTML = "I read this!"
-    readButton.addEventListener("click", notReadBook)
-    dateSection.setAttribute("id", "no-date-section")
-    readButton.setAttribute("id", "not-read-button")
- }
-
- //add not read status and remove date
-  function notReadBook(){
-    const readButton = document.getElementById("not-read-button")
-    const dateSection = document.getElementById("no-date-section")
-
+    readButton.addEventListener("click", function notReadBook(){
     readButton.innerHTML = "I have not read this."
-    dateSection.innerHTML = "Date Finshed: unfinished"
-    readButton.addEventListener("click", readBook)
-    readButton.setAttribute("id", "read-button")
-    dateSection.setAttribute("id", "date-section")
-  } 
-
-
+    bookComment.appendChild(dateSection)
+    dateSection.innerHTML = "Date Finished: unfinished"
+    })
+ }
 
 
 addBook.addEventListener("click", addTolibrary)
@@ -116,6 +109,7 @@ addBook.addEventListener("click", addTolibrary)
 
 
 
-
+//add function to toggle back to read this!
+//figure out removing or adding info to selected section
 
 
